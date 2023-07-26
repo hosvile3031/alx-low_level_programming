f56da427836f082c7af3a959ccd046e7e6c5df74
hosvile3031 <hosvile3031@gmail.com>
2023-07-25 20:01:11 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
 /**
- * print_most_numbers - prints numbers between 0 to 9
- * (neither 2 nor 4).
- * Return: no return.
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
-void print_most_numbers(void)
+void print_rev(char *s)
 {
-	int ch;
+	int longi = 0;
+	int o;
 
-	for (ch = 48; ch < 58; ch++)
+	while (*s != '\0')
 	{
-		if (ch != 50 && ch != 52)
-			_putchar(ch);
+		longi++;
+		s++;
 	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
 	_putchar('\n');
 }
